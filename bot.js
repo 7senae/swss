ffbea3e4311595261349357c4da4dcb6ef3b9d7f
johnmuheeb20052 <42880215+johnmuheeb20052@users.noreply.github.com>
2018-09-01 08:05:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -312,27 +312,26 @@ message.channel.send(`**#-  ${args}**`);
 
 
 
-client.on('message', function(msg) {
-  if(msg.content.startsWith ('$server')) {
-    if(!msg.channel.guild) return msg.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
-    let embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setThumbnail(msg.guild.iconURL)
-    .addField(':globe_with_meridians: **اسم السيرفر : **' , `**[ ${msg.guild.name} ]**`,true)
-    .addField(':earth_africa: ** موقع السيرفر :**',`**[ ${msg.guild.region} ]**`,true)
-    .addField(':military_medal:** الرتب :**',`**[ ${msg.guild.roles.size} ]**`,true)
-    .addField(':bust_in_silhouette:** عدد الاعضاء :**',`**[ ${msg.guild.memberCount} ]**`,true)
-    .addField(':white_check_mark:** عدد الاعضاء الاونلاين :**',`**[ ${msg.guild.members.filter(m=>m.presence.status == 'online').size} ]**`,true)
-    .addField(':pencil:** الرومات الكتابية :**',`**[ ${msg.guild.channels.filter(m => m.type === 'text').size} ]**`,true)
-    .addField(':loud_sound:** رومات الصوت :**',`**[ ${msg.guild.channels.filter(m => m.type === 'voice').size} ]**`,true)
-    .addField(':crown:** صاحب السيرفر :**',`**[ ${msg.guild.owner} ]**`,true)
-    .addField(':id:** ايدي السيرفر :**',`**[ ${msg.guild.id} ]**`,true)
-    .addField(':date:** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
-    msg.channel.send({embed:embed});
-  }
+client.on("message", message => {
+    
+    if(message.content.startsWith(prefix + "server")) {
+        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**ط§ظ„ط£ظ…ط± ظ„ظ„ط§ط¯ط§ط±ظ‡ ظپظ‚ط· **");
+        const starEmbed = new Discord.RichEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setThumbnail(message.guild.iconURL)
+        .setColor("RANDOM")
+.setDescription(`**
+طµط§ط­ط¨ ط§ظ„ط³ظٹط±ظپط±  :key: \` ${message.guild.owner.user.username} \`                                        
+ط£ط¹ط¶ط§ط، ط§ظ„ط³ظٹط±ظپط± :bar_chart: \` ${message.guild.memberCount}\`
+                                  ط±ظˆظ…ط§طھ ط§ظ„ط³ظٹط±ظپط± 
+\`#\`${message.guild.channels.filter(m => m.type === 'text').size} \`ًں”ˆ\`${message.guild.channels.filter(m => m.type === 'voice').size}
+ ط±طھط¨ ط§ظ„ط³ظٹط±ظپط± :scroll: 
+${message.guild.roles.size}
+**  `)
+        message.channel.send(starEmbed)
+    }
 });
-
-
+   
 
 
 
