4e9d032094236db776abc52a9387cf11eedbc06e
johnmuheeb20052 <42880215+johnmuheeb20052@users.noreply.github.com>
2018-09-23 18:06:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1819,4 +1819,39 @@ client.on('message', message => {
 }
 });
 
+
+
+  client.on('message', ReeBeL => {
+if(ReeBeL.content.startsWith(prefix + 'email')) {
+function randomem() {
+let email = '';
+const ReBeL = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._"\'';
+for (let i = 0; i < 5; i++) email += ReBeL.charAt(Math.floor(Math.random() * ReBeL.length));
+return email;
+}
+function randompass() {
+let pass = '';
+const CoDeS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.!?@#$%&()-_"\'';
+for (let i = 0; i < 8; i++) pass += CoDeS.charAt(Math.floor(Math.random() * CoDeS.length));
+return pass;
+}
+const random1 = randomem();
+const random2 = randompass();
+ReeBeL.channel.send(`email : **${random1}@gmail.com**
+password : **${random2}**`);
+}});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
