30b01f6982306fa58ddd2e54a7c61747d3be1c5e
johnmuheeb20052 <42880215+johnmuheeb20052@users.noreply.github.com>
2018-12-08 09:07:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,6 @@ const Canvas = require('canvas');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const ytdl = require('ytdl-core');
-const fs = require('fs');
 const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 const prefix = "-";
