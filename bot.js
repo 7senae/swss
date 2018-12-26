35cc9685d769e58e2d922d0530e8bc7968316c3c
johnmuheeb20052 <42880215+johnmuheeb20052@users.noreply.github.com>
2018-12-26 13:12:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2853,7 +2853,7 @@ let channel = member.guild.channels.find(c => c.name === 'welcome');
     .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
-}       });
+})       });
 
 
 client.on('guildMemberRemove', member => {
@@ -2883,7 +2883,7 @@ let channel = member.guild.channels.find(c => c.name === 'welcome');
     .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
-}       });
+})       });
 
 
 
