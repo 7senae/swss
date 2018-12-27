9e3b7f93a25bd8ddf97b85e7434ecceb1a9680bd
johnmuheeb20052 <42880215+johnmuheeb20052@users.noreply.github.com>
2018-12-27 10:07:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2803,7 +2803,7 @@ client.on('guildMemberAdd', member => {
         .setThumbnail(m.avatarURL)
         .setAuthor(m.username,m.avatarURL)
         .addField('Joined Discord From :',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-        .setDescription('welcome to ${guild.name}')
+        .setDescription('welcome to ${member.guild.name}')
          .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:yumz});          
          
